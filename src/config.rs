d5c13317693f12e5d53fe1c01760a640309b985d
jtong1231 <jtong5995@gmail.com>
2025-12-14 17:25:21 +0800

    Update rendezvous server and port constants

--- src/config.rs
+++ src/config.rs
@@ -100,12 +100,12 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cd2.nxkansa6.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cd2.nxkansa7.top"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
-pub const RENDEZVOUS_PORT: i32 = 45506;
-pub const RELAY_PORT: i32 = 45507;
-pub const WS_RENDEZVOUS_PORT: i32 =45508;
-pub const WS_RELAY_PORT: i32 = 45509;
+pub const RENDEZVOUS_PORT: i32 = 45606;
+pub const RELAY_PORT: i32 = 45607;
+pub const WS_RENDEZVOUS_PORT: i32 =45608;
+pub const WS_RELAY_PORT: i32 = 45609;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
